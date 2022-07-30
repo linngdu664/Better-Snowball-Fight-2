87ada33c9abcad5d66371de68f6b045b2627c3d6
zx1316 <1315862279@qq.com>
2022-07-30 17:09:26 +0800

    添加雪球近战事件

---
+++ src/main/java/com/linngdu664/bsf/EventHandler.java
@@ -0,0 +1,57 @@
+package com.linngdu664.bsf;
+
+import com.linngdu664.bsf.item.SmoothSnowballItem;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.monster.Blaze;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class EventHandler {
+    @SubscribeEvent
+    public void attackEntity(AttackEntityEvent event) {
+        Player player = event.getPlayer();
+        Entity entity = event.getTarget();
+        Level level = player.getLevel();
+        Item item = player.getMainHandItem().getItem();
+        if (!player.isSpectator() && entity instanceof LivingEntity target) {
+            if (item instanceof SolidBucketItem) {
+                target.setTicksFrozen(240);
+                target.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 100, 1));
+                target.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 20, 2));
+                target.addEffect(new MobEffectInstance(MobEffects.WEAKNESS, 150, 1));
+                if (!player.getAbilities().instabuild) {
+                    player.getItemInHand(InteractionHand.MAIN_HAND).shrink(1);
+                    player.getInventory().placeItemBackInInventory(new ItemStack(Items.BUCKET), true);
+                }
+                for (int i = 0; i < 32; i++) {
+                    level.addParticle(ParticleTypes.ITEM_SNOWBALL, target.getX(), target.getEyeY(), target.getZ(), 0, 0, 0);
+                }
+                if (target instanceof Blaze) {
+                    target.hurt(DamageSource.playerAttack(player), 8);
+                }
+            } else if (item instanceof SnowballItem || item instanceof SmoothSnowballItem) {
+                target.setTicksFrozen(180);
+                target.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 20, 1));
+                target.addEffect(new MobEffectInstance(MobEffects.WEAKNESS, 30, 1));
+                if (!player.getAbilities().instabuild) {
+                    player.getItemInHand(InteractionHand.MAIN_HAND).shrink(1);
+                }
+                for (int i = 0; i < 16; i++) {
+                    level.addParticle(ParticleTypes.ITEM_SNOWBALL, target.getX(), target.getEyeY(), target.getZ(), 0, 0, 0);
+                }
+                if (target instanceof Blaze) {
+                    target.hurt(DamageSource.playerAttack(player), 4);
+                }
+            }
+        }
+    }
+}
--- src/main/java/com/linngdu664/bsf/Main.java
+++ src/main/java/com/linngdu664/bsf/Main.java
@@ -1,6 +1,7 @@
 package com.linngdu664.bsf;
 
 import com.linngdu664.bsf.item.setter.ItemRegister;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -11,5 +12,6 @@ public class Main {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         ItemRegister.ITEMS.register(bus);
         SoundRegister.SOUNDS.register(bus);
+        MinecraftForge.EVENT_BUS.register(new EventHandler());
     }
 }
