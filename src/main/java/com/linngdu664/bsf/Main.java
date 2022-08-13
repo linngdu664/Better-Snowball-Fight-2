557ff977bd19c0cdc13390698a5d07a68fdc4e82
陈鲁强 <664774652@qq.com>
2022-08-13 20:38:54 +0800

    使用时切换雪球炮和手套的贴图

--- src/main/java/com/linngdu664/bsf/Main.java
+++ src/main/java/com/linngdu664/bsf/Main.java
@@ -1,9 +1,13 @@
 package com.linngdu664.bsf;
 
+import com.linngdu664.bsf.item.SnowballCannonItem;
 import com.linngdu664.bsf.item.setter.ItemRegister;
+import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("bsf")
@@ -11,7 +15,44 @@ public class Main {
     public Main() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         ItemRegister.ITEMS.register(bus);
+        bus.addListener(this::initClient);
         SoundRegister.SOUNDS.register(bus);
         MinecraftForge.EVENT_BUS.register(new EventHandler());
+    }
+    private void initClient(final FMLClientSetupEvent event){
+        event.enqueueWork(()->{
+            ItemProperties.register(ItemRegister.SNOWBALL_CANNON.get(),
+                    new ResourceLocation("pull"),(itemStack,world,livingEntity,num)->{
+                        if(livingEntity==null){
+                            return 0.0F;
+                        }else{
+                            return livingEntity.getUseItem() != itemStack ? 0.0F : (float)(itemStack.getUseDuration() - livingEntity.getUseItemRemainingTicks()) / 20.0F;
+                        }
+                    });
+            ItemProperties.register(ItemRegister.SNOWBALL_CANNON.get(),new ResourceLocation("pulling"),(itemStack, world, livingEntity, num)
+                    ->livingEntity != null && livingEntity.isUsingItem() && livingEntity.getUseItem() == itemStack ? 1.0F : 0.0F);
+            ItemProperties.register(ItemRegister.FREEZING_SNOWBALL_CANNON.get(),
+                    new ResourceLocation("pull"),(itemStack,world,livingEntity,num)->{
+                        if(livingEntity==null){
+                            return 0.0F;
+                        }else{
+                            return livingEntity.getUseItem() != itemStack ? 0.0F : (float)(itemStack.getUseDuration() - livingEntity.getUseItemRemainingTicks()) / 20.0F;
+                        }
+                    });
+            ItemProperties.register(ItemRegister.FREEZING_SNOWBALL_CANNON.get(),new ResourceLocation("pulling"),(itemStack, world, livingEntity, num)
+                    ->livingEntity != null && livingEntity.isUsingItem() && livingEntity.getUseItem() == itemStack ? 1.0F : 0.0F);
+            ItemProperties.register(ItemRegister.POWERFUL_SNOWBALL_CANNON.get(),
+                    new ResourceLocation("pull"),(itemStack,world,livingEntity,num)->{
+                        if(livingEntity==null){
+                            return 0.0F;
+                        }else{
+                            return livingEntity.getUseItem() != itemStack ? 0.0F : (float)(itemStack.getUseDuration() - livingEntity.getUseItemRemainingTicks()) / 20.0F;
+                        }
+                    });
+            ItemProperties.register(ItemRegister.POWERFUL_SNOWBALL_CANNON.get(),new ResourceLocation("pulling"),(itemStack, world, livingEntity, num)
+                    ->livingEntity != null && livingEntity.isUsingItem() && livingEntity.getUseItem() == itemStack ? 1.0F : 0.0F);
+            ItemProperties.register(ItemRegister.GLOVE.get(),new ResourceLocation("using"),(itemStack, world, livingEntity, num)
+                    ->livingEntity != null && livingEntity.isUsingItem() && livingEntity.getUseItem() == itemStack ? 1.0F : 0.0F);
+        });
     }
 }
