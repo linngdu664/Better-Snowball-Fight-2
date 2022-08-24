73da49772074317bd5261781f323dcf8b091345a
zx1316 <1315862279@qq.com>
2022-08-24 14:25:14 +0800

    优化代码

--- src/main/java/com/linngdu664/bsf/mixin/SnowballMixin.java
+++ src/main/java/com/linngdu664/bsf/mixin/SnowballMixin.java
@@ -22,12 +22,14 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Snowball.class)
 public class SnowballMixin extends ThrowableItemProjectile {
@@ -57,9 +59,10 @@ public class SnowballMixin extends ThrowableItemProjectile {
         }
     }
 
-    @ModifyConstant(method = "onHit", constant = @Constant(intValue = 3))
-    private int injected(int value) {
-        return 0;
+    @Inject(method = "onHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;broadcastEntityEvent(Lnet/minecraft/world/entity/Entity;B)V"), cancellable = true)
+    private void injectedOnHit(HitResult pResult, CallbackInfo ci) {
+        this.discard();
+        ci.cancel();
     }
 
     private boolean isHeadingToSnowball(Player player) {
