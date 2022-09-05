f8f13de1ae45050bdd536f4e6eeead33713b5156
陈鲁强 <664774652@qq.com>
2022-09-05 09:58:15 +0800

    重命名

--- src/main/java/com/linngdu664/bsf/item/snowball/ForceSnowballItem.java
+++ src/main/java/com/linngdu664/bsf/item/snowball/ForceSnowballItem.java
@@ -1,11 +1,16 @@
 package com.linngdu664.bsf.item.snowball;
 
+import com.linngdu664.bsf.entity.AdvancedSnowballEntity;
 import com.linngdu664.bsf.item.setter.ItemRegister;
+import com.linngdu664.bsf.util.SnowballType;
 import com.linngdu664.bsf.util.Target;
 import com.linngdu664.bsf.util.TrackingSnowballMode;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -25,6 +30,8 @@ public class ForceSnowballItem extends Item {
     @Override
     public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level pLevel, Player pPlayer, @NotNull InteractionHand pUsedHand) {
         ItemStack itemStack = pPlayer.getItemInHand(pUsedHand);
+
+        //Putting snowballs into empty storage tanks
         if (pPlayer.getOffhandItem().getItem() == ItemRegister.EMPTY_SNOWBALL_STORAGE_TANK.get()) {
             if (target == Target.MONSTER) {
                 if (mode == TrackingSnowballMode.GRAVITY) {
@@ -45,7 +52,9 @@ public class ForceSnowballItem extends Item {
             if (!pPlayer.getAbilities().instabuild) {
                 itemStack.shrink(pPlayer.getMainHandItem().getCount());
             }
-        } else if ((pPlayer.getOffhandItem().getItem() == ItemRegister.GRAVITY_SNOWBALL_TO_MONSTER_STORAGE_TANK.get() && target == Target.MONSTER && mode == TrackingSnowballMode.GRAVITY ||
+        }
+        //Putting snowballs into corresponding storage tanks
+        else if ((pPlayer.getOffhandItem().getItem() == ItemRegister.GRAVITY_SNOWBALL_TO_MONSTER_STORAGE_TANK.get() && target == Target.MONSTER && mode == TrackingSnowballMode.GRAVITY ||
                 pPlayer.getOffhandItem().getItem() == ItemRegister.REPULSION_SNOWBALL_TO_MONSTER_STORAGE_TANK.get() && target == Target.MONSTER && mode == TrackingSnowballMode.REPULSION ||
                 pPlayer.getOffhandItem().getItem() == ItemRegister.GRAVITY_SNOWBALL_TO_PROJECTILE_STORAGE_TANK.get() && target == Target.PROJECTILE && mode == TrackingSnowballMode.GRAVITY ||
                 pPlayer.getOffhandItem().getItem() == ItemRegister.REPULSION_SNOWBALL_TO_PROJECTILE_STORAGE_TANK.get() && target == Target.PROJECTILE && mode == TrackingSnowballMode.REPULSION ||
@@ -62,10 +71,29 @@ public class ForceSnowballItem extends Item {
                 }
                 pPlayer.getOffhandItem().setDamageValue(0);
             }
+        }else {
+            //todo this facking shit unfinished
+            pLevel.playSound(null, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(), SoundEvents.SNOWBALL_THROW, SoundSource.NEUTRAL, 0.5F, 0.4F / (pLevel.getRandom().nextFloat() * 0.4F + 0.8F));
+            if (!pLevel.isClientSide) {
+                float i = pPlayer.hasEffect(MobEffects.WEAKNESS) ? 0.75F : 1.0F;
+                float j = pPlayer.hasEffect(MobEffects.WEAKNESS) ? 0.5F : 1.0F;
+                AdvancedSnowballEntity snowballEntity = new AdvancedSnowballEntity(pLevel, pPlayer, SnowballType.ICE, 3.0F * j, 6.0F * j);
+                snowballEntity.setItem(itemStack);
+                snowballEntity.shootFromRotation(pPlayer, pPlayer.getXRot(), pPlayer.getYRot(), 0.0F, 1.125F * i, 1.0F);
+                snowballEntity.frozenTicks = 60;
+
+
+
+                pLevel.addFreshEntity(snowballEntity);
+            }
+            if (!pPlayer.getAbilities().instabuild) {
+                itemStack.shrink(1);
+            }
         }
-        pPlayer.awardStat(Stats.ITEM_USED.get(this));
+
+
+        pPlayer.awardStat(Stats.ITEM_USED.get(this));//Feedback effect
         return InteractionResultHolder.sidedSuccess(itemStack, pLevel.isClientSide());
     }
-    //todo function of force effect
 
 }
