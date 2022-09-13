5162c4b3963ae5e94aea6c032213c4d14efda319
zx1316 <1315862279@qq.com>
2022-09-13 18:16:20 +0800

    添加急冻雪球方块特效

--- src/main/java/com/linngdu664/bsf/entity/snowball/nomal_snowball/FrozenSnowballEntity.java
+++ src/main/java/com/linngdu664/bsf/entity/snowball/nomal_snowball/FrozenSnowballEntity.java
@@ -3,15 +3,24 @@ package com.linngdu664.bsf.entity.snowball.nomal_snowball;
 import com.linngdu664.bsf.entity.BSFSnowballEntity;
 import com.linngdu664.bsf.item.ItemRegister;
 import com.linngdu664.bsf.util.LaunchFunc;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.LiquidBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.HitResult;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
+import static com.linngdu664.bsf.util.BSFMthUtil.modSqr;
+import static com.linngdu664.bsf.util.TargetGetter.getTargetList;
+
 public class FrozenSnowballEntity extends BSFSnowballEntity {
-    public float frozenRange=2.5f;
+    public float frozenRange=2.5F;
     public FrozenSnowballEntity(LivingEntity livingEntity, Level level, LaunchFunc launchFunc) {
         super(livingEntity, level);
         this.setFrozenTime(60).setLaunchFrom(launchFunc.getLaunchForm()).setDamage(3).setBlazeDamage(8);
@@ -35,7 +44,36 @@ public class FrozenSnowballEntity extends BSFSnowballEntity {
     protected void onHit(@NotNull HitResult pResult) {
         super.onHit(pResult);
         if (!isCaught) {
-            //todo complete here
+            BlockPos blockPos = new BlockPos(pResult.getLocation());
+            for (int i = (int) (blockPos.getX() - frozenRange); i <= blockPos.getX() + frozenRange; i++) {
+                for (int j = (int) (blockPos.getY() - frozenRange); j <= blockPos.getY() + frozenRange; j++) {
+                    for (int k = (int) (blockPos.getZ() - frozenRange); k <= blockPos.getZ() + frozenRange; k++) {
+                        if (modSqr(i - blockPos.getX(), j - blockPos.getY(), k - blockPos.getZ()) <= frozenRange * frozenRange) {
+                            BlockPos blockPos1 = new BlockPos(i, j, k);
+                            BlockState blockState = level.getBlockState(blockPos1);
+                            if (blockState.getBlock() == Blocks.WATER && blockState.getValue(LiquidBlock.LEVEL) == 0) {
+                                BlockState blockState1 = Blocks.ICE.defaultBlockState();
+                                level.setBlock(blockPos1, blockState1, 3);
+                            } else if (blockState.getBlock() == Blocks.LAVA && blockState.getValue(LiquidBlock.LEVEL) == 0) {
+                                BlockState blockState1 = Blocks.BASALT.defaultBlockState();
+                                level.setBlock(blockPos1, blockState1, 3);
+                            } else if (blockState.getBlock() == Blocks.FIRE) {
+                                BlockState blockState1 = Blocks.AIR.defaultBlockState();
+                                level.setBlock(blockPos1, blockState1, 3);
+                            } else if (blockState.getBlock() == Blocks.AIR && level.getBlockState(blockPos1.below()).getMaterial().isSolid()) {
+                                BlockState blockState1 = Blocks.SNOW.defaultBlockState();
+                                level.setBlock(blockPos1, blockState1, 3);
+                            }
+                        }
+                    }
+                }
+            }
+            List<LivingEntity> list = getTargetList(this, LivingEntity.class, 2.5F);
+            for (LivingEntity entity : list) {
+                if (distanceToSqr(entity) < frozenRange * frozenRange) {
+                    //todo:add entity effects here
+                }
+            }
         }
         if (!level.isClientSide) {
             this.discard();
