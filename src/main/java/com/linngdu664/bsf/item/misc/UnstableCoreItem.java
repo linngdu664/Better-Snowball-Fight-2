84bafa7602bea0fb3b9b4a404cd0bf603715c0aa
陈鲁强 <664774652@qq.com>
2022-09-15 20:16:59 +0800

    设置不稳定核心裂变配方

--- src/main/java/com/linngdu664/bsf/item/misc/UnstableCoreItem.java
+++ src/main/java/com/linngdu664/bsf/item/misc/UnstableCoreItem.java
@@ -1,11 +1,35 @@
 package com.linngdu664.bsf.item.misc;
 
+import com.linngdu664.bsf.item.ItemRegister;
 import com.linngdu664.bsf.util.ItemGroup;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 
 public class UnstableCoreItem extends Item {
     public UnstableCoreItem() {
-        super(new Properties().tab(ItemGroup.MAIN).rarity(Rarity.UNCOMMON));
+        super(new Properties().tab(ItemGroup.MAIN).rarity(Rarity.EPIC));
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext context) {
+        Player player = context.getPlayer();
+        InteractionHand usedItemHand = player.getUsedItemHand();
+        Level level = context.getLevel();
+        Block block = level.getBlockState(context.getClickedPos()).getBlock();
+        ItemStack itemStack = player.getItemInHand(usedItemHand);
+        if (block == Blocks.LODESTONE){
+            itemStack.shrink(1);
+            player.getInventory().placeItemBackInInventory(new ItemStack(ItemRegister.GRAVITY_CORE.get(), 1), true);
+            player.getInventory().placeItemBackInInventory(new ItemStack(ItemRegister.REPULSION_CORE.get(), 1), true);
+        }
+        return super.useOn(context);
     }
 }
