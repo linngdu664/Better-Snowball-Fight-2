563bab4507f8394350ff02ffabd28fe9fc7f95a3
zx1316 <1315862279@qq.com>
2022-09-26 23:16:54 +0800

    修正错误的tooltip

--- src/main/java/com/linngdu664/bsf/item/snowball/normal_snowball/SmoothSnowballItem.java
+++ src/main/java/com/linngdu664/bsf/item/snowball/normal_snowball/SmoothSnowballItem.java
@@ -63,9 +63,9 @@ public class SmoothSnowballItem extends BSFSnowballItem {
     @Override
     public void appendHoverText(@NotNull ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, @NotNull TooltipFlag pIsAdvanced) {
         pTooltipComponents.add(new TranslatableComponent("lunch_yes_hand.tooltip").withStyle(ChatFormatting.DARK_GREEN));
-        pTooltipComponents.add(new TranslatableComponent("lunch_yes_cannon.tooltip").withStyle(ChatFormatting.DARK_GREEN));
-        pTooltipComponents.add(new TranslatableComponent("lunch_yes_machine_gun.tooltip").withStyle(ChatFormatting.DARK_GREEN));
-        pTooltipComponents.add(new TranslatableComponent("lunch_yes_shotgun.tooltip").withStyle(ChatFormatting.DARK_GREEN));
+        pTooltipComponents.add(new TranslatableComponent("lunch_no_cannon.tooltip").withStyle(ChatFormatting.DARK_GREEN));
+        pTooltipComponents.add(new TranslatableComponent("lunch_no_machine_gun.tooltip").withStyle(ChatFormatting.DARK_GREEN));
+        pTooltipComponents.add(new TranslatableComponent("lunch_no_shotgun.tooltip").withStyle(ChatFormatting.DARK_GREEN));
         pTooltipComponents.add(new TranslatableComponent("smooth_snowball.tooltip").withStyle(ChatFormatting.BLUE));
     }
 }
