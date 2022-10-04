15c59a236a36d3ca2bd8e22f36454ec9f089c49a
linngdu664 <664774652@qq.com>
2022-10-04 14:49:54 +0800

    目标定位器发射铁雪球

--- src/main/java/com/linngdu664/bsf/entity/snowball/nomal_snowball/GPSSnowballEntity.java
+++ src/main/java/com/linngdu664/bsf/entity/snowball/nomal_snowball/GPSSnowballEntity.java
@@ -4,7 +4,11 @@ import com.linngdu664.bsf.entity.BSFSnowballEntity;
 import com.linngdu664.bsf.item.ItemRegister;
 import com.linngdu664.bsf.item.tool.TargetLocatorItem;
 import com.linngdu664.bsf.util.LaunchFrom;
+import net.minecraft.Util;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -18,13 +22,13 @@ public class GPSSnowballEntity extends BSFSnowballEntity {
     public GPSSnowballEntity(LivingEntity livingEntity, Level level, TargetLocatorItem targetLocator) {
         super(livingEntity, level);
         this.targetLocator = targetLocator;
-        this.setPunch(2.0).setLaunchFrom(LaunchFrom.HAND);
+        this.setPunch(2.0).setDamage(2).setLaunchFrom(LaunchFrom.HAND);
         this.setItem(new ItemStack(ItemRegister.GPS_SNOWBALL.get()));
     }
 
     @Override
     protected Item getCorrespondingItem() {
-        return ItemRegister.COMPACTED_SNOWBALL.get();
+        return ItemRegister.IRON_SNOWBALL.get();
     }
 
     @Override
@@ -40,6 +44,9 @@ public class GPSSnowballEntity extends BSFSnowballEntity {
         super.onHitEntity(pResult);
         if (!isCaught && pResult.getEntity() instanceof LivingEntity livingEntity) {
             targetLocator.setLivingEntity(livingEntity);
+            if (getOwner() instanceof Player) {
+                getOwner().sendMessage(new TextComponent(livingEntity.toString()), Util.NIL_UUID);
+            }
         }
     }
 }
--- src/main/java/com/linngdu664/bsf/item/tool/TargetLocatorItem.java
+++ src/main/java/com/linngdu664/bsf/item/tool/TargetLocatorItem.java
@@ -4,7 +4,9 @@ import com.linngdu664.bsf.entity.BSFSnowballEntity;
 import com.linngdu664.bsf.entity.snowball.nomal_snowball.GPSSnowballEntity;
 import com.linngdu664.bsf.item.ItemRegister;
 import com.linngdu664.bsf.item.snowball.normal.CompactedSnowballItem;
+import com.linngdu664.bsf.item.snowball.normal.IronSnowballItem;
 import com.linngdu664.bsf.item.tank.normal.CompactedSnowballStorageTank;
+import com.linngdu664.bsf.item.tank.normal.IronSnowballStorageTank;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
@@ -21,7 +23,7 @@ public class TargetLocatorItem extends BSFEnhanceableToolItem {
     private LivingEntity livingEntity;
 
     public TargetLocatorItem() {
-        super(Rarity.UNCOMMON, 256);
+        super(Rarity.UNCOMMON, 512);
     }
 
     @Override
@@ -29,8 +31,8 @@ public class TargetLocatorItem extends BSFEnhanceableToolItem {
         ItemStack itemStack = pPlayer.getItemInHand(pUsedHand);
         int i;
         for (i = 0; i < pPlayer.getInventory().getContainerSize(); i++) {
-            if (pPlayer.getInventory().getItem(i).getItem() instanceof CompactedSnowballItem ||
-                    pPlayer.getInventory().getItem(i).getItem() instanceof CompactedSnowballStorageTank) {
+            if (pPlayer.getInventory().getItem(i).getItem() instanceof IronSnowballItem ||
+                    pPlayer.getInventory().getItem(i).getItem() instanceof IronSnowballStorageTank) {
                 break;
             }
         }
@@ -43,7 +45,7 @@ public class TargetLocatorItem extends BSFEnhanceableToolItem {
             }
             if (!pPlayer.getAbilities().instabuild) {
                 ItemStack stack = pPlayer.getInventory().getItem(i);
-                if (stack.getItem() instanceof CompactedSnowballItem) {
+                if (stack.getItem() instanceof IronSnowballItem) {
                     stack.shrink(1);
                     if (stack.isEmpty()) {
                         pPlayer.getInventory().removeItem(stack);
