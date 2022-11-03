6fcbb5251f2eff515793bf85305927ed149a0257
zx1316 <1315862279@qq.com>
2022-11-03 12:38:17 +0800

    重命名类

--- src/main/java/com/linngdu664/bsf/enchantment/EnchantmentRegister.java
+++ src/main/java/com/linngdu664/bsf/enchantment/EnchantmentRegister.java
@@ -1,11 +1,12 @@
 package com.linngdu664.bsf.enchantment;
 
+import com.linngdu664.bsf.Main;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-public class BSFEnchantments {
-    public static final DeferredRegister<Enchantment> REGISTRY = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, "bsf");
+public class EnchantmentRegister {
+    public static final DeferredRegister<Enchantment> REGISTRY = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, Main.MODID);
     public static final RegistryObject<Enchantment> KINETIC_ENERGY_STORAGE = REGISTRY.register("kinetic_energy_storage", KineticEnergyStorageEnchantment::new);
 }
