2b11986d5401cd4fe90feaf04a947cc18f72ba86
zx1316 <1315862279@qq.com>
2022-11-05 17:29:43 +0800

    删import

--- src/main/java/com/linngdu664/bsf/item/tool/TargetLocatorItem.java
+++ src/main/java/com/linngdu664/bsf/item/tool/TargetLocatorItem.java
@@ -15,7 +15,6 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
